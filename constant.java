36f2b26889cef6d93c83be9dd1a9ad6e46e4fda0
swetavaddoriya <31240558+swetavaddoriya@users.noreply.github.com>
2019-11-19 11:06:50 +0530

    Create constant.java

---
+++ constant.java
@@ -0,0 +1,18 @@
+
+public class Constants {
+
+    public static String Local = "http://192.168.1.134:6464/";
+    public static String Live = "http://api.gdpimpex.com/";
+
+    public static String baseURL = BuildConfig.DEBUG ? Live : Live;
+
+    public static String jsonUrl = "http://www.gdpimpex.com/";
+
+    public static final String PROVIDER = "com.oozeetech.gdpapplication.fileprovider";
+    public static final String ISLOGIN = "islogin";
+    public static final String LOGIN_REPONSE = "login_reponse";
+    public static final String APIKEY = "123";
+    public static final String OBJECT = "object";
+    public static final String SHARE = "share";
+    public static final String ID = "id";
+}
